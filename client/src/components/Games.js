ba0c3f410c810ddc38a51c0b7cc1cbbd003139a2
kolbymckeown <mckeownkolby@gmail.com>
2020-10-22 00:51:15 -0400

    Minor fix

--- client/src/components/Games.js
+++ client/src/components/Games.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const API_KEY = process.env.REACT_APP_STEAM_WEB_KEY;
+const API_KEY = process.env.REACT_APP_STEAM_API_KEY;
 
 const Games = () => {
 	React.useEffect(() => {
